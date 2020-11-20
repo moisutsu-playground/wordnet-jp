48ea730b932b31e8f88366fb8ce67da8c66af9a3
moisutsu <moisutsu@gmail.com>
2020-11-20 17:05:04 +0900

    Add impl for generate hypernym

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from argparse import ArgumentParser
 
 from wordnet_json import load_words
-from wordnet_db import getSynonym
+from wordnet_db import getSynonym, getHypernym
 
 
 def main(args):
@@ -13,6 +13,13 @@ def main(args):
                 if len(synonym) == 0:
                     continue
                 f.write(f"{word}\t{' '.join(synonym)}\n")
+    elif args.mode == "hypernym":
+        with Path(args.output_path).open("w") as f:
+            for word in load_words():
+                hypernym = getHypernym(word)
+                if len(hypernym) == 0:
+                    continue
+                f.write(f"{word}\t{' '.join(hypernym)}\n")
     else:
         raise ValueError(f"No such mode '{args.mode}''")
 
--- wordnet_db.py
+++ wordnet_db.py
@@ -49,18 +49,46 @@ def getWordsFromSenses(sense, lang="jpn"):
 def getSynonym(word):
     synonym = {}
     words = getWords(word)
+    print(words)
     if words:
         for w in words:
             sense = getSenses(w)
+            print(sense)
             s = getWordsFromSenses(sense)
             synonym = dict(list(synonym.items()) + list(s.items()))
     synonym = sorted({x for v in synonym.values() for x in v if x != word})
     return synonym
 
+# Key: 下位語(str), value: 上位語(List[str])
+hierarchy_dict = None
+
+def init_hierarchy_dict():
+    global hierarchy_dict
+    hierarchy_dict = {}
+    cur = conn.execute("select synset1,synset2 from synlink where link='hypo'")
+    for row in cur:
+        n_term = row[0]
+        b_term = row[1]
+
+        if b_term not in hierarchy_dict:
+            hierarchy_dict[b_term] = []
+
+        hierarchy_dict[b_term].append(n_term)
+
 def getHypernym(word):
-    hypernym = {}
+    if hierarchy_dict is None:
+        init_hierarchy_dict()
+    hypernym = []
     words = getWords(word)
     if words:
         for w in words:
-            pass
+            senses = getSenses(w)
+            for sense in senses:
+                if not sense.synset in hierarchy_dict:
+                    return []
+                hypersynsets = hierarchy_dict[sense.synset]
+                for hypersynset in hypersynsets:
+                    hyper_words = getWordsFromSynset(hypersynset, "jpn")
+                    hypernym.extend([hyper_word.lemma for hyper_word in hyper_words if hyper_word.lemma != word])
+    hypernym = list(set(hypernym))
     return hypernym
